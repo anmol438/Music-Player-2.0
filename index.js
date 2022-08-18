cb1936b6f9c5b6c7bed9be19ebc534f5c53e8778
Anmol Gupta <iitkanmol438@gmail.com>
2022-08-19 03:30:10 +0530

    Final Project Deployment

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.use(flashMware);
 
 app.use('/',  require('./routes/index'));
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, (err) => {
     if(err){
         console.log(`Error in listening to express server at port : ${port}`);
